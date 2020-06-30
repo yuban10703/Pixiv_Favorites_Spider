c693285eaafd5d1bf1a5b1e029ab31f6c9918957
yuban10703 <2846021566@qq.com>
2020-06-30 20:53:11 +0800

    emm

--- select/download.py
+++ select/download.py
@@ -26,71 +26,65 @@ headers = {'User-Agent': 'PixivAndroidApp/5.0.191 (Android 6.0.1; HUAWEI ALE-CL0
            'App-Version': '5.0.191',
            'Referer': 'https://www.pixiv.net'}
 
-list = os.listdir(path)  # 获取下载路径的所有文件
-list_original = os.listdir(path_original)
-
-
 
 async def download(session, filename, url, path):  # 下载
     try:
         async with session.get(url, headers=headers) as res:
-            if res.status == 200:
-                date = await res.content.read()
-                Image.open(io.BytesIO(date)).save(path + filename)  # 以二进制读取文件,并转码为对应格式保存
-                print('{}下载成功  '.format(filename, res.status))
-                return
-            else:
-                faild_list.append(url)
-                print('下载失败: >>>{}<<< {}'.format(filename, res.status))
-                return
+            assert res.status == 200
+            date = await res.content.read()
+            Image.open(io.BytesIO(date)).save(path + filename)  # 以二进制读取文件,并转码为对应格式保存
+            print('{}下载成功 :{}'.format(filename, res.status))
+            return
     except:  # 不知道为什么会出错.....
-        faild_list.append(url)
         print('下载失败: >>>{}<<<'.format(filename))
+        faild_list.append(url)
         return
 
 
 async def download_original(session, filename, url, path):  # 下载
     try:
         async with session.get(url, headers=headers) as res:
-            if res.status == 200:  # 状态码为200才保存
-                date = await res.content.read()
-                async with aiofiles.open(path + filename, 'wb') as f:
-                    await f.write(date)
-                print('{}下载成功  '.format(filename, res.status))
-                return
-            else:
-                faild_list.append(url)
-                print('下载失败: >>>{}<<< {}'.format(filename, res.status))
-                return
-    except:
-        faild_list.append(url)
+            assert res.status == 200
+            date = await res.content.read()
+            async with aiofiles.open(path + filename, 'wb') as f:
+                await f.write(date)
+            print('{}下载成功  '.format(filename, res.status))
+            return
+    except:  # 不知道为什么会出错.....
         print('下载失败: >>>{}<<<'.format(filename))
+        faild_list.append(url)
         return
 
-
 alldata = mycol.find({})  # 读取数据库所有信息
-tasks = []  # 任务列表
 faild_list = []
+task_data = []
 
-
+for data in alldata:  # 遍历数据库里所有信息
+    task_data.append({'url': data['large'], 'url_original': data['original'], 'filename': data['filename']})
+print('获取数据库成功~')
 async def main():
-    # num = 0
-    async with aiohttp.ClientSession() as session:
-        for data in alldata:  # 遍历数据库里所有信息
-            # if num >=100:
-            #     break
-            url = data['large']
-            url_original = data['original']
-            filename = data['filename']  # 获取文件名
-            if filename not in list:
-                tasks.append(asyncio.create_task(download(session, filename, url, path)))  # 加入任务
-            if filename not in list_original:
-                tasks.append(
-                    asyncio.create_task(download_original(session, filename, url_original, path_original)))  # 加入任务(原图)
-                # num +=1
-        await asyncio.gather(*tasks)  # 并发执行
+    conn = aiohttp.TCPConnector(limit=50)  # 限制连接池
+    async with aiohttp.ClientSession(connector=conn) as session:
+        for _ in range(3):
+            tasks = []  # 任务列表
+            faild_list.clear()
+            list = os.listdir(path)  # 获取下载路径的所有文件
+            list_original = os.listdir(path_original)
+            for data in task_data:
+                url = data['url']
+                url_original = data['url_original']
+                filename = data['filename']  # 获取文件名
+                if filename not in list:
+                    tasks.append(asyncio.create_task(download(session, filename, url, path)))  # 加入任务
+                if filename not in list_original:
+                    tasks.append(
+                        asyncio.create_task(download_original(session, filename, url_original, path_original)))  # 加入任务(原图)
+            await asyncio.gather(*tasks)  # 并发执行
+            if not len(faild_list): #如果没有下载失败的就结束
+                break
 
-
-asyncio.run(main())  # gogogo
-print(faild_list)
-print('下载失败数量:"{}'.format(len(faild_list)))
+if __name__ == '__main__':
+    print('开始下载~')
+    asyncio.run(main())  # gogogo
+    print(faild_list)
+    print('下载失败数量:{}'.format(len(faild_list)))
