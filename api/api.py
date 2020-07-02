ce9b6c4d84bd557b19748f734fb0edd1191329ae
yuban10703 <2846021566@qq.com>
2020-07-02 21:42:33 +0800

    emmmm

--- api/api.py
+++ api/api.py
@@ -1,7 +1,7 @@
 import re
 import motor.motor_asyncio
 from fastapi import FastAPI, Query
-from fastapi.responses import JSONResponse
+from fastapi.responses import ORJSONResponse
 
 app = FastAPI()
 client = motor.motor_asyncio.AsyncIOMotorClient
@@ -23,14 +23,13 @@ async def find(condition, num):  # collection为str参数
 
 # ------------------------------------------------------------------------------------------------------------
 
-@app.get("/setu")
+@app.get("/setu", response_class=ORJSONResponse)
 async def setu_v1(tag: str = Query('', max_length=35), r18: bool = False):
     print('{0}SETU_V1: tag:[{1}] r18:[{2}]{3}'.format('>' * 20, tag, r18, '<' * 20))
     condition = {}
     try:
         if (len(tag) != 0) and (not tag.isspace()):  # 如果tag不为空(字符串字数不为零且不为空)
-            data_re = re.compile(tag.strip())  # 去除字符串两边空格
-            condition['tags'] = data_re
+            condition['tags'] = re.compile(tag.strip())  # 正则并去掉字符两边空格
         if r18:
             condition['type'] = 'porn'
             setu = await find(condition, 1)
@@ -43,22 +42,21 @@ async def setu_v1(tag: str = Query('', max_length=35), r18: bool = False):
             setu_full['code'] = 200
             return setu_full
         else:
-            return JSONResponse(status_code=404, content={'code': 404, 'msg': '色图库中没找到色图~'})
+            return ORJSONResponse(status_code=404, content={'code': 404, 'msg': '色图库中没找到色图~'})
     except Exception as error:
-        return JSONResponse(status_code=500, content={'code': 500, 'msg': '爆炸啦~', 'error': str(error)})
+        return ORJSONResponse(status_code=500, content={'code': 500, 'msg': '爆炸啦~', 'error': str(error)})
 
 
 # ------------------------------------------------------------------------------------------------------------
 
 
-@app.get("/setu_v2")
+@app.get("/setu_v2", response_class=ORJSONResponse)
 async def setu_v2(tag: str = Query('', max_length=45), num: int = Query(1, ge=1, le=10), r18: bool = False):
     print('{0}SETU_V2: tag:[{1}] r18:[{2}] num:[{3}]{4}'.format('>' * 20, tag, r18, num, '<' * 20))
     condition = {}
     try:
         if (len(tag) != 0) and (not tag.isspace()):  # 如果tag不为空(字符串字数不为零且不为空)
-            data_re = re.compile(tag.strip())
-            condition['tags'] = data_re
+            condition['tags'] = re.compile(tag.strip())
         if r18:
             condition['type'] = 'porn'
             setu = await find(condition, num)
@@ -73,27 +71,29 @@ async def setu_v2(tag: str = Query('', max_length=45), num: int = Query(1, ge=1,
             setu_full['data'] = setu
             return setu_full
         else:
-            return JSONResponse(status_code=404, content={'code': 404, 'msg': '色图库中没找到色图~'})
+            return ORJSONResponse(status_code=404, content={'code': 404, 'msg': '色图库中没找到色图~'})
     except Exception as error:
-        return JSONResponse(status_code=500, content={'code': 500, 'msg': '爆炸啦~', 'error': str(error)})
+        return ORJSONResponse(status_code=500, content={'code': 500, 'msg': '爆炸啦~', 'error': str(error)})
 
 
 # ------------------------------------------------------------------------------------------------------------
 
-alltag = re.compile('.')
-ways_v3 = {0: 'normal', 1: 'sexy', 2: 'porn', 3: alltag}
+ways_v3 = {
+    0: {'type': 'normal'},
+    1: {'type': 'sexy'},
+    2: {'type': 'porn'},
+    3: {'$or': [{'type': 'normal'}, {'type': 'sexy'}, {'type': 'porn'}]}
+}
 
 
-@app.get("/setu_v3")
+@app.get("/setu_v3", response_class=ORJSONResponse)
 async def setu_v3(tag: str = Query('', max_length=45), num: int = Query(1, ge=1, le=10),
                   type: int = Query(0, ge=0, le=3)):
     print('{0}SETU_V3: tag:[{1}] type:[{2}] num:[{3}]{4}'.format('>' * 20, tag, type, num, '<' * 20))
     try:
-        if len(tag) == 0 or tag.isspace():  # 如果tag为空(字符串字数为0或者为空格)
-            condition = {'type': ways_v3[type]}
-        else:
-            data_re = re.compile(tag.strip())  # 正则
-            condition = {'tags': data_re, 'type': ways_v3[type]}
+        condition = ways_v3[type]
+        if (len(tag) != 0) and (not tag.isspace()):  # 如果tag不为空(字符串字数不为零且不为空)
+            condition['tags'] = re.compile(tag.strip())
         setu = await find(condition, num)
         setus_num = len(setu)
         if setus_num != 0:
@@ -101,7 +101,7 @@ async def setu_v3(tag: str = Query('', max_length=45), num: int = Query(1, ge=1,
             setu_full['data'] = setu
             return setu_full
         else:
-            return JSONResponse(status_code=404, content={'code': 404, 'count': 0, 'msg': '色图库中没找到色图~'})
+            return ORJSONResponse(status_code=404, content={'code': 404, 'count': 0, 'msg': '色图库中没找到色图~'})
     except Exception as error:
-        return JSONResponse(status_code=500,
-                            content={'code': 500, 'count': 0, 'msg': '爆炸啦~', 'error': str(error)})
+        return ORJSONResponse(status_code=500,
+                              content={'code': 500, 'count': 0, 'msg': '爆炸啦~', 'error': str(error)})
